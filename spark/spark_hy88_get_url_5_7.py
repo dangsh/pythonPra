b6f2e6c192264d6c27995dad4aebcc2c82fbec0c
dangsh <954316227@qq.com>
2018-05-07 15:26:59 +0800

    update spark in 5_7

--- spark/spark_hy88_get_url_5_7.py
+++ spark/spark_hy88_get_url_5_7.py
@@ -31,19 +31,28 @@ def map_func(x):
             res.add("http:"+i)
         return list(res)
     else:
-	return []
+        return []
 rdd_more = rdd.flatMap(map_func).distinct().filter(lambda x: x != None)
 rdd_more.persist(StorageLevel(True,True,False,False))
-
-print rdd_more.count()
-rdd1 = spark.read.format("com.mongodb.spark.sql.DefaultSource").option("uri","mongodb://192.168.14.90/zkp.huangye88_product_url").load().rdd.map(lambda x:x._id)
-rdd2 = spark.read.format("com.mongodb.spark.sql.DefaultSource").option("uri","mongodb://192.168.14.90/final_field.refine_hy88").load().rdd.map(lambda x:x.url)
-rdd4 = spark.read.format("com.mongodb.spark.sql.DefaultSource").option("uri","mongodb://192.168.14.90/hy88.url_5_2").load().rdd.map(lambda x:x.url)
+def map_func3(x):
+    import json
+    x = json.loads(x)
+    return x['_id']
+rdd1 = sc.textFile('/huangye88_product_url.json').map(map_func3)
+def map_func(x):
+    import json
+    x = json.loads(x)
+    return x['url']
+rdd2 = sc.textFile('/refine_hy88.json').map(map_func)
+def map_func2(x):
+    import json
+    x = json.loads(x)
+    return x['_id']
+rdd4 = sc.textFile('/url_5_2.json').map(map_func2)
 rdd3 = rdd1.union(rdd2)
 rdd3 = rdd3.union(rdd4)
 rdd3 = rdd3.distinct().filter(lambda x: x != None)
 rdd3.persist(StorageLevel(True,True,False,False))
-
 print rdd3.count()
 final_rdd = rdd_more.subtract(rdd3)
 final_rdd = final_rdd.map(lambda x: (x,))
